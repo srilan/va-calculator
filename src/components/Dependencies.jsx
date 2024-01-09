86d64aebb9ffd44deefebea8e40729ee7b56f8c2
Lorenzo Miguel Dela Cruz <miguel.cruz@GTPH-LPC097.cscc.local>
2024-01-10 02:49:40 +0800

    add dependency calculator

--- src/components/Dependencies.jsx
+++ src/components/Dependencies.jsx
@@ -12,12 +12,17 @@ function Dependencies({
   MaritalStatusClicked,
   aidAndAttendanceClicked,
   dependentParentsClicked,
+  calculatedRating,
+  disabilityRating,
 }) {
   return (
-    <div>
-      <div class="bg-white">
-        <div class="flex flex-col md:flex-row justify-center w-full">
-          <div class="flex flex-col w-full px-6  mt-5">
+    <div class="px-6 pt-5 mt-5">
+      <div class="relative bg-white border-t-4 border-[#184997] px-6 pt-5">
+        <div class="absolute right-2/4 translate-x-2/4 lg:translate-x-0 top-[-40px] bebas bg-[#184997] flex px-3 text-xl pt-2 pb-1 text-white rounded-t-xl tracking-wider">
+          STEP 2
+        </div>
+        <div class="flex flex-col md:flex-row justify-center w-full ">
+          <div class="flex flex-col w-full pe-6 mt-5">
             <div class="flex w-full justify-center items-center bebas text-4xl lg:text-5xl">
               <h2 class="mx-auto" style="color: #000000;">
                 TO COMPLETE THE ASSESSMENT,
@@ -37,25 +42,25 @@ function Dependencies({
             <div class="bebas text-3xl my-2 lg:my-5">
               <h2 class="mx-auto text-3xl lg:text-4xl" style="color: #000000;">
                 COMBINED DISABILITY PERCENTAGE:
-                <span style="color: #184997"> 0% </span>
+                <span style="color: #184997"> {calculatedRating}% </span>
               </h2>
             </div>
             <div class="bebas text-3xl my-2 lg:my-5">
               <h2 class="mx-auto text-3xl lg:text-4xl" style="color: #000000;">
                 CURRENT DISABILITY RATING:
-                <span style="color: #184997"> 0% </span>
+                <span style="color: #184997"> {disabilityRating}% </span>
               </h2>
             </div>
             <div class="bebas text-3xl my-2 lg:my-5">
               <h2 class="mx-auto text-3xl lg:text-4xl" style="color: #000000;">
-                MONTHLY PAYMOUNT AMOUNT:
+                MONTHLY PAYMENT AMOUNT:
                 <span style="color: #184997"> {monthlyPayment}$ </span>
               </h2>
             </div>
           </div>
 
           {/* SECOND PANELO TO HAHAHA */}
-          <div class="flex flex-col justify-between w-full px-6 my-5">
+          <div class="flex flex-col justify-between w-full ps-6 my-5">
             <div class="flex flex-row" style="border-top: 2px solid #b52d38;">
               <div class="my-2">
                 <div
